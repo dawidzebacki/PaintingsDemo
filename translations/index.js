a91340338ce4d19551526e9b798e325b13773f28
Dawid Zebacki <dawidzebacki@protonmail.com>
2021-04-03 14:56:31 +0200

    add: new info about Darek

--- translations/index.js
+++ translations/index.js
@@ -16,7 +16,7 @@ export const resources = {
           "I paint pictures on canvas, I will be happy to paint something for you.",
         ItemThreeTitle: "About me",
         ItemThreeText:
-          "I'm 50 this year and I am an amateur painter. Painting has always fascinated me, only life took a different path and there was no time for painting. I love to paint, it calms me down and at the same time I do something beautiful. I like to paint nature and culture and also something abstract. Life is my inspiration. I love colors, but also details in painted pictures, so that these pictures give life and energy and reflect the Norwegian spirit. ",
+          "My name is Darek, I'm 50 this year and I'm an amateur painter. Painting has always fascinated me, only life took a different path and there was no time for painting. I love to paint, it calms me down and at the same time I do something beautiful. I like to paint nature and culture and also something abstract. Life is my inspiration. I love colors, but also details in painted pictures, so that these pictures give life and energy and reflect the Norwegian spirit. Currently, I am a member of Gjøvik Kunstforening.",
       },
       Gallery: {
         Title: "Some of my work.",
@@ -45,7 +45,7 @@ export const resources = {
           "Maluję obrazy na płótnie. Chętnie wykonam coś specjalnie dla Ciebie.",
         ItemThreeTitle: "Coś o mnie",
         ItemThreeText:
-          "W tym roku kończę 50 lat i jestem malarzem amatorem. Malarstwo zawsze mnie fascynowało, tylko życie poszło inną drogą i nie było czasu na malowanie. Uwielbiam malować, to mnie uspokaja, a jednocześnie robię coś pięknego. Lubię malować naturę i kulturę, a także coś abstrakcyjnego. Życie jest moją inspiracją. Uwielbiam kolory, ale też detale w malowanych obrazach, aby te obrazy dodawały życia i energii oraz odzwierciedlały norweskiego ducha.",
+          "Mam na imię Darek, w tym roku kończę 50 lat i jestem malarzem amatorem. Malarstwo zawsze mnie fascynowało, tylko życie poszło inną drogą i nie było czasu na zajmowanie się tym co lubię. Uwielbiam malować, to mnie uspokaja, a jednocześnie robię coś pięknego. W swoich pracah lubię uwieczniać naturę, kulturę, a także coś abstrakcyjnego. Życie jest moją inspiracją. Uwielbiam kolory, ale też zwracam uwagę na najmniejsze detale. Chcę aby moja twórczość dodawała chęci życia i energii oraz odzwierciedlały norweskiego ducha. Aktualnie udzielam się też w stowarzyszeniu Gjøvik Kunstforening.",
       },
       Gallery: {
         Title: "Niektóre z moich prac.",
@@ -67,13 +67,13 @@ export const resources = {
         Title: "Legg til en farge i livet ditt.",
         ItemOneTitle: "Bildebehandling",
         ItemOneText:
-        "Jeg lager datagrafikk, retusjering og bildebehandling.",
-      ItemTwoTitle: "Maling av bilder",
+          "Jeg lager datagrafikk, retusjering og bildebehandling.",
+        ItemTwoTitle: "Maling av bilder",
         ItemTwoText:
           "Jeg maler bilder på lerret, jeg vil gjerne male noe for deg. ",
         ItemThreeTitle: "Om meg",
         ItemThreeText:
-          "Jeg er 50. Jeg er en amatørmaler. Maleri har alltid fascinert meg, bare livet tok en annen vei og det var ikke tid for maling. Jeg elsker å male, det roer meg og samtidig gjør jeg noe vakkert. Jeg liker å male natur og kultur og også noe abstrakt. Livet er min inspirasjon. Jeg elsker farger, men også detaljer i malte bilder, slik at disse bildene gir liv og energi og gjenspeiler den norske ånden.",
+          "Jeg heter Darek, jeg er 50 år og er amatørmaler. Maleri har alltid fascinert meg, bare livet tok en annen vei og det var ikke tid for maling. Jeg elsker å male, det roer meg og samtidig gjør jeg noe vakkert. Jeg liker å male natur og kultur og også noe abstrakt. Livet er min inspirasjon. Jeg elsker farger, men også detaljer i malte bilder, slik at disse bildene gir liv og energi og gjenspeiler den norske ånden. Jeg er for tiden medlem av Gjøvik Kunstforening.",
       },
       Gallery: {
         Title: "Noe av arbeidet mitt.",
