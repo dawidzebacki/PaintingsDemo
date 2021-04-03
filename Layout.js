cda8779336a46b5c09f856eb5ffe2a46759324b3
Dawid Zebacki <dawidzebacki@protonmail.com>
2021-04-03 14:34:14 +0200

    fix: next/head

--- Layout.js
+++ Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Head } from "next/head";
+import Head from "next/head";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 // import { initGA, logPageView } from "./analytics";
